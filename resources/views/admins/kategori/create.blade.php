e42da8a44bd9f673b419642327df3e0c27bb99af
vagrant <vagrant@homestead>
2017-02-02 13:50:00 +0000

    add notif error

--- resources/views/admins/kategori/create.blade.php
+++ resources/views/admins/kategori/create.blade.php
@@ -7,6 +7,13 @@
 	<div class="row">
 		<div class="col-md-4 col-md-offset-4">
 			<h1>Tambah Kategori</h1>
+			@if(count($errors) > 0)
+				<div class="alert alert-danger">
+					@foreach($errors->all() as $error)
+						<p>{{$error}}</p>
+					@endforeach
+				</div>
+			@endif
 			<form action="{{route('kategori.store')}}" method="post">
 			{{csrf_field()}}
 				<div class="form-group">
