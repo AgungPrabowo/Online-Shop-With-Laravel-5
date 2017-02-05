28f360ce7dae61af1961535a55473afa83624bf6
vagrant <vagrant@homestead>
2017-02-05 16:42:42 +0000

    change column kategoris_id to kategori_id

--- database/migrations/2017_02_02_141517_create_products_table.php
+++ database/migrations/2017_02_02_141517_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('kategoris_id');
+            $table->integer('kategori_id');
             $table->string('name');
             $table->integer('price');
             $table->text('description');
