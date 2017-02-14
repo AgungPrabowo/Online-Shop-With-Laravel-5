4c75803810a07b05d8e79fbbf9f147bd6db0c186
vagrant <vagrant@homestead>
2017-02-14 16:28:23 +0000

    added paginate product

--- resources/views/admins/home.blade.php
+++ resources/views/admins/home.blade.php
@@ -2,7 +2,6 @@ Hello,
 @if(Sentinel::check())
 Your Logged, Your Name is {{Sentinel::getUser()->first_name}}
 @endif
-{{storage_path('app/public')}}
-<a href="{{route('AdminGetLogout')}}">Logout</a>
-<a href="{{route('kategori.index')}}">Kategori</a>
-<a href="{{route('product.index')}}">Product</a>
+<li><a href="{{route('kategori.index')}}">Kategori</a></li>
+<li><a href="{{route('product.index')}}">Product</a></li>
+<li><a href="{{route('AdminGetLogout')}}">Logout</a></li>
